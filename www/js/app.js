d06d6ce38ddfb755abc4afd225efc591c10a5061
syqs <kristian.magda@gmail.com>
2016-09-29 19:48:24 -0500

    hello

--- www/js/app.js
+++ www/js/app.js
@@ -48,7 +48,7 @@ angular.module('starter', ['ionic', 'satellizer', 'ionic-datepicker', 'ionic-tim
   };
 
   if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
-    commonConfig.redirectUri = 'http://localhost:8100/';
+    commonConfig.redirectUri = 'http://shift-ninja.herokuapp.com/';
   }
   $authProvider.facebook(angular.extend({}, commonConfig, {
     clientId: '1169374106434305',
