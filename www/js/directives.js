ccce10e24cbb299fcdc34c09c93bb805728579ad
Kristian Magda <syqs@users.noreply.github.com>
2016-10-05 11:48:38 -0500

    Revert "New shift ctrl"

--- www/js/directives.js
+++ www/js/directives.js
@@ -46,7 +46,7 @@ angular.module('starter.directives', [])
   var approvedShift = false;
   var notificationMsg = "";
 
-  var userApprovals = []; //all the pickup shifts
+  var userApprovals; //all the pickup shifts
   var shifts;
   var stores;
   var myStore;
@@ -281,16 +281,22 @@ angular.module('starter.directives', [])
         return myRequests;
       });
     },
-    getShiftsPosted: function(){
-      return $http.get(BASE_URL + 'myshifts')
-      .then(function(response) {
-        return response.data;
+    GetMyShifts: function(){
+      return $http({
+          method: 'GET',
+          url: BASE_URL+'myshifts'
+      }).then(function(response) {
+        something = response.data;
+        return something;
       });
     },
-    getShiftsPicked: function(){
-      return $http.get(BASE_URL + 'shiftsIPickedUp')
-      .then(function(response) {
-        return response.data;
+    GetShiftsIPickedUp: function(){
+      return $http({
+          method: 'GET',
+          url: BASE_URL+'shiftsIPickedUp'
+      }).then(function(response) {
+        iamworking = response.data;
+        return iamworking;
       });
     },
     setPartnerId: function(id, shift, code, pickShift){
@@ -370,30 +376,6 @@ angular.module('starter.directives', [])
 })
 
 
-// .factory('ShiftFactory', function($http) {
-
-//   var urlbase = 'https://shift-it.herokuapp.com/';
-  
-//   var sf = {};
-
-//   sf.getShiftsPosted = function(){
-//     return $http.get(urlbase + 'myshifts')
-//     .then(function(response) {
-//       return response.data;
-//     });
-//   };
-
-//   sf.getShiftsPicked = function(){
-//     return $http.get(urlbase + 'shiftsIPickedUp')
-//     .then(function(response) {
-//       console.log("gsp response: ", response)
-//       return response.data;
-//     });
-//   };
-
-//   return sf;
-
-// })
 
 
 
