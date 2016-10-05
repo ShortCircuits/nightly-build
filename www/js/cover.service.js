1af93ec7a9e48d2fc9e3ed66e86e5c71cc58f800
Chad Griffis <cmg123@me.com>
2016-10-05 18:18:38 -0500

    continue refactor

--- www/js/cover.service.js
+++ www/js/cover.service.js
@@ -16,12 +16,16 @@
       covered : false
     };
 
+  // This function converts the minutes into a 2 digit number if 0 is chosen
     var convertMinutes = function(minutes) {
       if (minutes === 0) {
         return "00"
       }
       return minutes;
     };
+
+  // This is the Date picker modal popout, that initializes the shift_start and shift_end keys in the shift object
+  // On a chosen date it sets both values to the chosen date with no time, and then it shows the first time picker
 
     var ipObj1 = {
       callback: function(val) { //Mandatory
@@ -30,9 +34,9 @@
         var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         console.log("shiftData is: ", shift);
         console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-        CoverService.openTimePicker1();
+        openTimePicker1();
         shiftDate = shift.shift_start.getUTCDate() + " " + monthNames[shift.shift_start.getUTCMonth()] + " " + shift.shift_start.getUTCFullYear()
-
+        $rootScope.$broadcast('update');
         // var month = dateObj.getUTCMonth() + 1; //months from 1-12
         // var day = dateObj.getUTCDate();
         // var year = dateObj.getUTCFullYear();
@@ -55,6 +59,8 @@
       templateType: 'popup' //Optional
     };
 
+  // This is the modal for the end shift time picker, it will update the shift object with the correct time in the
+  // current time zone for the user. On submit it opens the prize picker modal.
     var ipObj2 = {
       callback: function(val) { //Mandatory
         if (typeof(val) === 'undefined' || shift.shift_start === undefined) {
@@ -68,8 +74,9 @@
           console.log(shift);
           console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
         }
-        CoverService.prizePicker();
+        $rootScope.$broadcast('prizeMode')
         endTime = selectedTime.getUTCHours() + ":" + convertMinutes(selectedTime.getUTCMinutes());
+        $rootScope.$broadcast('update');
       },
       inputTime: 50400, //Optional
       format: 12, //Optional
@@ -77,6 +84,8 @@
       setLabel: 'Set2' //Optional
     };
 
+  // This is the modal for the start shift time picker, it will update the shift object with the correct time in the
+  // current time zone for the user. On submit it opens the end shift time picker modal.
     var ipObj3 = {
       callback: function(val) { //Mandatory
         if (typeof(val) === 'undefined' || shift.shift_start === undefined) {
@@ -90,8 +99,9 @@
           console.log("our built time is: ", splitStart);
           console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
         }
-        CoverService.openTimePicker2();
+        openTimePicker2();
         startTime = selectedTime.getUTCHours() + ":" + convertMinutes(selectedTime.getUTCMinutes());
+        $rootScope.$broadcast('update');
       },
       inputTime: 50400, //Optional
       format: 12, //Optional
@@ -99,23 +109,48 @@
       setLabel: 'Set2' //Optional
     };
 
+    var makeTextTime = function(data) {
+      var textStart = data.shift_start;
+      var textEnd = data.shift_end;
+      var p1 = textStart.toDateString();
+      var p2 = textStart.toLocaleTimeString();
+      p1 = p1.slice(0, -5);
+      p2 = p2.slice(0, -6) + p2.slice(-3);
+      var p3 = textEnd.toLocaleTimeString();
+      p3 = p3.slice(0, -6) + p3.slice(-3);
+      return p1 + " from " + p2 + " to " + p3;
+    };
+
+    var openTimePicker1 = function() {
+      ionicTimePicker.openTimePicker(ipObj3);
+    };
+
+    var openTimePicker2 = function() {
+      ionicTimePicker.openTimePicker(ipObj2);
+    };
 
     return {
+
+      openTimePicker1 : openTimePicker1,
+
+      openTimePicker2 : openTimePicker2,
 
       setHomeLocForShift : function() {
         $http({
           method: 'GET',
           url: 'https://shift-it.herokuapp.com/getProfileInfo'
         }).then(function successCallback(response) {
+          console.log("this is the response.data from the setHome: ", response.data)
           home_store = response.data[0].home_store;
           submitted_by_name = response.data[0].firstName + " " + response.data[0].lastName;
+          console.log("look in here for the home_store", home_store);
           $rootScope.$broadcast('update');
         }, function errorCallback(response) {
           console.log("Failed to set home location in CoverCtrl");
         });
       },
 
-      postShift : function(data) {
+      postShift : function() {
         if (shiftDate === undefined) {
           alert("Please enter a date for your shift!")
         } else if (startTime === undefined) {
@@ -123,7 +158,8 @@
         } else if (endTime === undefined) {
           alert("Please enter an end time for your shift!")
         } else {
-          shift.shift_text_time = CoverService.makeTextTime(shift); 
+          shift.shift_text_time = makeTextTime(shift); 
+          console.log("look here I found the shift: ", shift);
           $http({
             method: 'POST',
             url: 'https://shift-it.herokuapp.com/shifts',
@@ -137,28 +173,8 @@
         }
       },
 
-      makeTextTime : function(data) {
-        var textStart = data.shift_start;
-        var textEnd = data.shift_end;
-        var p1 = textStart.toDateString();
-        var p2 = textStart.toLocaleTimeString();
-        p1 = p1.slice(0, -5);
-        p2 = p2.slice(0, -6) + p2.slice(-3);
-        var p3 = textEnd.toLocaleTimeString();
-        p3 = p3.slice(0, -6) + p3.slice(-3);
-        return p1 + " from " + p2 + " to " + p3;
-      },
-
       openDatePicker : function() {
         ionicDatePicker.openDatePicker(ipObj1);
-      },
-
-      openTimePicker1 : function() {
-        ionicTimePicker.openTimePicker(ipObj3);
-      },
-
-      openTimePicker2 : function() {
-        ionicTimePicker.openTimePicker(ipObj2);
       },
 
       increment : function() {
@@ -173,29 +189,30 @@
         }
       },
 
-      prizePicker : function(modal) {
-        modal.show();
-      },
-
-      // Function to submit the prize to the shift object
-      addPrize : function(prizeAmount) {
-        shift.prize = "$" + prizeAmount + ".00";
-        console.log(shift);
-        CoverService.closePrize();
-      },
-
-      // Function to close the prize modal
-      closePrize : function() {
-        $scope.modal.hide();
+      addPrize : function() {
+        $rootScope.$broadcast('update');
+        shift.prize = "$" + prize + ".00";
       },
 
       shift : function() {
         return shift;
-      }
+      },
 
       prize : function() {
         return prize;
-      }
+      },
+
+      shiftDate : function() {
+        return shiftDate;
+      },
+
+      startTime : function() {
+        return startTime;
+      },
+
+      endTime : function() {
+        return endTime;
+      },
 
     } 
   }
