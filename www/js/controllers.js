79b00f0fd44e465ae89dfed7cc1aa10bc3a23e47
syqs <kristian.magda@gmail.com>
2016-10-07 17:33:06 -0500

    removed some commented out code from controller.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -62,47 +62,6 @@ angular.module('starter.controllers', [])
     });
 
   })
-  // .controller('ProfileCtrl', function($scope, $http, $ionicModal, UserService, ProfileService) {
-
-  //   $scope.$on('update', function() {
-  //     $scope.data = {
-  //       profile : ProfileService.getProfileData(),
-  //       editProfile : ProfileService.getEditProfile(),
-  //     }
-  //   });
-
-  //   $scope.$on('$ionicView.enter', function() {
-  //     if (!UserService.isAuthenticated()) {
-  //       window.location = '#/lobby'
-  //     }
-  //     ProfileService.getUserData();
-
-  //     // Open and close the modal to edit Profile
-  //     $ionicModal.fromTemplateUrl('templates/editProfile.html', {
-  //       scope: $scope
-  //     }).then(function(modal) {
-  //       $scope.modal = modal;
-  //     });
-
-  //     // Triggered in the edit profile modal to close it
-  //     $scope.closeEditProfile = function() {
-  //       $scope.modal.hide();
-  //     };
-
-  //     // Open the edit profile modal
-  //     $scope.openEditProfile = function() {
-  //       ProfileService.fillEditProfile();
-  //       $scope.modal.show();
-  //     };
-
-  //     // Functionality for editProfile modal
-  //     $scope.submitProfile = function() {
-  //       ProfileService.submitProfile()
-  //       $scope.closeEditProfile();
-  //     };
-
-  //   });
-  // })
 
 .controller('PickupCtrl', function($scope, UserService, $ionicLoading, PickupService) {
 
