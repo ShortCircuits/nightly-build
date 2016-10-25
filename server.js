927a0d6c16253e7a64775af6dac737349ce99413
syqs <kristian.magda@gmail.com>
2016-10-25 17:28:58 -0500

    latest up updates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.all('*', function(req, res, next) {
 // API Routes
 // app.get('/blah', routeHandler);
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
